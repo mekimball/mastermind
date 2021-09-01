f1782d9b7f8c1fb8488f714bdfa676676c3fbd71
Matthew Kimball <mekimba6@gmail.com>
2021-09-01 16:14:41 -0600

    adds placements class and tests

---
+++ lib/placement.rb
@@ -0,0 +1,21 @@
+class Placement
+  attr_reader :pegs
+
+  def initialize(pegs)
+    @pegs = pegs
+    @set = []
+  end
+
+  def place_peg(pegs)
+    pegs.count.times do
+      @set << pegs.shuffle[0]
+    end
+    @set
+  end
+
+  def set_details
+    @set.map do |peg|
+      peg.initial
+    end
+  end
+end
---
+++ spec/placement_spec.rb
@@ -0,0 +1,40 @@
+require './lib/color'
+require './lib/pegs'
+require './lib/placement'
+
+RSpec.describe Placement do
+  it 'exists' do
+    peg1 = Pegs.new("blue", "B")
+    pegs = [peg1]
+    placement = Placement.new(pegs)
+    expect(placement).to be_a(Placement)
+  end
+  it 'can have pegs' do
+    peg1 = Pegs.new("blue", "B")
+    pegs = [peg1]
+    placement = Placement.new(pegs)
+    expect(placement.pegs).to eq([peg1])
+  end
+
+  it 'can place pegs' do
+    peg1 = Pegs.new("blue", "B")
+    # peg2 = Pegs.new("red", "R")
+    # peg3 = Pegs.new(yellow)
+    # peg4 = Pegs.new(green)
+    pegs = [peg1, peg1, peg1, peg1]
+    placement = Placement.new(pegs)
+    expect(placement.place_peg(pegs)).to eq([peg1, peg1, peg1, peg1])
+  end
+
+  it 'can list pegs' do
+    peg1 = Pegs.new("blue", "B")
+    # peg2 = Pegs.new(red)
+    # peg3 = Pegs.new(yellow)
+    # peg4 = Pegs.new(green)
+    pegs = [peg1, peg1, peg1, peg1]
+    # pegs.shuffle!(random: Random.new(1))
+    placement = Placement.new(pegs)
+    placement.place_peg(pegs)
+    expect(placement.set_details).to eq(["B", "B" ,"B", "B"])
+  end
+end

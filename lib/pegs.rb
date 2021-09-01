2d4a8d1511baabfd27476fdbd421129166299b86
Matthew Kimball <mekimba6@gmail.com>
2021-09-01 16:14:00 -0600

    adds pegs class and tests

---
+++ lib/pegs.rb
@@ -0,0 +1,9 @@
+class Pegs
+  attr_reader :color,
+              :initial
+
+  def initialize(color, initial)
+    @color = color
+    @initial = initial
+  end
+end
---
+++ spec/pegs_spec.rb
@@ -0,0 +1,23 @@
+require './lib/color'
+require './lib/pegs'
+
+RSpec.describe Pegs do
+  it 'exists' do
+    peg = Pegs.new("blue", "B")
+    expect(peg).to be_a(Pegs)
+  end
+
+  it 'has colors' do
+    peg1 = Pegs.new("blue", "B")
+    peg2 = Pegs.new("red", "R")
+    expect(peg1.color).to eq("blue")
+    expect(peg2.color).to eq("red")
+  end
+
+  it 'has initals' do
+    peg1 = Pegs.new("blue", "B")
+    peg2 = Pegs.new("red", "R")
+    expect(peg1.initial).to eq("B")
+    expect(peg2.initial).to eq("R")
+  end
+end

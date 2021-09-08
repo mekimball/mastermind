d0076156684e1031f61c3c289a0346c6197a92bb
Matthew Kimball <mekimba6@gmail.com>
2021-09-07 22:00:10 -0600

    cleans up code

--- mastermind.rb
+++ mastermind.rb
@@ -5,9 +5,12 @@ require './lib/game'
 
 
 
-peg1 = Pegs.new("blue", "B")
-pegs = [peg1, peg1, peg1, peg1]
+peg1 = Pegs.new("yellow", "Y")
+peg2 = Pegs.new("red", "r")
+peg3 = Pegs.new("blue", "b")
+peg4 = Pegs.new("green", "g")
+pegs = [peg1, peg2, peg3, peg4]
 placement = Placement.new(pegs)
-game = Game.new(peg1)
+game = Game.new(pegs)
 placement.place_peg(pegs)
 game.start
